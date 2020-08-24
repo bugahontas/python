25fc7f67f8f2b247306eca69bcd661386fba7252
Helena M <contatohelmaqui@gmail.com>
2020-08-24 14:18:49 -0300

    Create MaiorEMenor1.py

---
+++ fundamentos/MaiorEMenor1.py
@@ -0,0 +1,8 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
+print()
+
+print(f'Maior número = {max(n1, n2, n3)}')
+print(f'Menor número = {min(n1, n2, n3)}')
+
