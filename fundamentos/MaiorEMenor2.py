7bc8ea1f9ed92f73fd10aac402fb4a7e9dbf30da
Helena M <contatohelmaqui@gmail.com>
2020-08-24 14:25:58 -0300

    Create MaiorEMenor2.py

---
+++ fundamentos/MaiorEMenor2.py
@@ -0,0 +1,12 @@
+# Verifica o maior e o menor número digitados em uma lista
+
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+n3 = int(input('Terceiro número: '))
+print()
+
+lista = [n1, n2, n3]
+
+print(f'Maior número = {max(lista)}')
+print(f'Menor número = {min(lista)}')
+
