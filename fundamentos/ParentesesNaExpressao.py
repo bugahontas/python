ee3a0e4bb3e01e85005167e4b525cbfd39381ac8
Helena M <contatohelmaqui@gmail.com>
2020-09-06 18:31:54 -0300

    Update ParentesesNaExpressao.py

--- fundamentos/ParentesesNaExpressao.py
+++ fundamentos/ParentesesNaExpressao.py
@@ -1,20 +1,17 @@
-# Parênteses na expressão: verifica se há fechamento de parênteses em uma expressão.
-## Ex1.: digite () e o programa irá mostrar 'Expressão correta'
-## Ex2.: digite ())) e o programa irá mostrar erro - 'Parênteses não fecham'
-## Ex3.: digite )() e o programa irá mostrar erro - 'Expressão deve começar com '(''.
-# ATENÇÃO: digite apenas parênteses. 
+# Parênteses na expressão: verifica se há fechamento de parênteses em uma expressão matemática.
 
-exp = input('Digite os parênteses: ')
+exp = input('Digite a expressão: ')
 
 if exp[0] == ')':
     while True:
         print()
         print('--> ERRO! A expressão deve começar com "(".')
-        exp = input('Digite novamente os parênteses: ')
+        exp = input('Digite novamente a expressão: ')
         if exp[0] == '(':
             break
 
 print()
+exp = ''.join(exp) # Para eliminar possíveis espaços vazios deixados pelo usuário.
 tam = len(exp) - 1
 
 tot = 0
@@ -22,12 +19,12 @@ c = 0
 while c <= tam:
     if exp[c] == '(':
         tot = tot + 1
-    else:
+    elif exp[c] == ')':
         tot = tot - 1
     c = c + 1
 
 print('=' * 10,'RESULTADO', '=' * 10)
-if tot == 0:
+if tot == 0: # Para cada abre-parênteses (+1) tem que ter um fecha-parêntese (-1), sempre resultando em 0.
     print('                     Expressão correta!')
-else:
+else: # Se o tot final for diferente de 0, é porque algum parêntese não foi fechado.
     print('Expressão errada! Parênteses não fecham.')
