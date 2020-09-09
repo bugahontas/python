f7771bc56c3b925e9a24ee3c59b7409a6ff1c1b0
Helena M <contatohelmaqui@gmail.com>
2020-09-09 19:22:16 -0300

    Create OrdemDecrescente.py

---
+++ fundamentos/OrdemDecrescente.py
@@ -0,0 +1,29 @@
+lista = []
+
+qtd = int(input('Criar lista com quantos números? '))
+print()
+
+c1 = 0
+while c1 < qtd:
+    n = float(input(f'Digite o {c1 + 1}o. número: '))
+    lista.append(n)
+    c1 = c1 + 1
+
+print()
+print(f'Lista criada = {lista}')
+
+c1 = 0
+c2 = 1
+aux = 0
+while c1 <= qtd - 2:
+    while c2 <= qtd - 1:
+        if lista[c1] < lista[c2]:
+            aux = lista[c1]
+            lista[c1] = lista[c2]
+            lista[c2] = aux
+        c2 = c2 + 1
+    c1 = c1 + 1
+    c2 = c1 + 1
+
+print()
+print(f'Nova lista em ordem decrescente = {lista}')
