2a26718d49b6bb964d2f55742ac6263aa77c844d
Helena M <contatohelmaqui@gmail.com>
2020-10-04 14:27:28 -0300

    Update CronometroSimples.py

--- fundamentos/CronometroSimples.py
+++ fundamentos/CronometroSimples.py
@@ -1,18 +1,21 @@
-# Cronômetro simples: conta de 0 até 20 minutos em contagem progressiva.
-# Altere a linha 'if m == 20:' do código e insira outro valor da sua preferência.
-# Este código aceita valores de 1 a 59 minutos.
+# Cronômetro simples: conta de 0 até 1 hora em contagem progressiva.
+# Altere a linha 'if h == 1:' do código e insira outro valor da sua preferência.
 
 from time import sleep
 
 s = 0
 m = 0
+h = 0
 aux = 0
 while s < 60:
-    sleep(1)
-    print('%02d' % m, ':', '%02d' % s)
-    if m == 20:
+    sleep(0.01)
+    print('%02d' % h, ':', '%02d' % m, ':', '%02d' % s)
+    if h == 1:
         break
     s = s + 1
-    if s % 60 == 0:
+    if s == 60:
         m = m + 1
         s = 0
+    if m  == 60:
+        h = h + 1
+        m = 0
