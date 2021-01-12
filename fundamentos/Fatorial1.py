842951358f4ecd6b4cc83d7324fd80fd49e25208
Helena M <contatohelmaqui@gmail.com>
2021-01-12 11:11:37 -0300

    Create Fatorial1.py

---
+++ fundamentos/Fatorial1.py
@@ -0,0 +1,12 @@
+# Fatorial1
+
+def fatorial(n):
+    fat = 1
+    while n > 1:
+        fat = fat * n
+        n = n - 1
+    return fat
+
+n = int(input('Fatorial do número: '))
+print()
+print(f'{n}! = {fatorial(n)}')
