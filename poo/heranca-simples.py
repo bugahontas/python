5b78dca0fdc814f33552d943629a0966c91c321a
Helena M <contatohelmaqui@gmail.com>
2021-02-28 15:05:14 -0300

    Update heranca-simples.py

--- poo/heranca-simples.py
+++ poo/heranca-simples.py
@@ -32,16 +32,22 @@ def cabecalho():
     numero = randint(1, 100000) #Número gerado será a chave do dicionário, que é a forma de identificar cada pedido
     print(f'    PEDIDO no. {numero}')
     return numero
+
+def quantosPedidos():
+    return int(input('Registrar quantos pedidos? '))
     
 def main():
     todos_pedidos = {} #Dicionário que armazena todos os pedidos
-    for contador in range(1, 3): 
+    for contador in range(1, quantosPedidos() + 1): 
         separador()
         Numero = cabecalho()
         separador()
         ped = Pedido() #Instanciação de cada pedido
         todos_pedidos[Numero] = [ped.cliente, ped.endereco, ped.fone, ped.mail, ped.produto, ped.valor]
-    print()
+        print()
+    separador()
+    print('         RESUMO')
+    separador()
     for chave, valor in todos_pedidos.items(): #Método items() de dicionários lista cada chave e seus valores 
         print(f'PEDIDO No.{chave} CLIENTE {valor[0]} ENTREGAR EM {valor[1]} TELEFONE {valor[2]} E-MAIL {valor[3]} PRODUTO {valor[4]} TOTAL R${valor[5]:.2f}')
         print()
