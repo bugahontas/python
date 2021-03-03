09d770a09491f9bdf21e70bdc3fb81e24e7a0b24
Helena M <contatohelmaqui@gmail.com>
2021-03-03 12:11:18 -0300

    Update oo-na-obra.py

--- poo/oo-na-obra.py
+++ poo/oo-na-obra.py
@@ -22,28 +22,23 @@ def topo(elemento = '', caractere = '*', qtd = 10):
         print(caractere * qtd)
     print()
 
-def unidadeMedida():
-    print('Tecle [m] para METRO')
-    print('Tecle [cm] para CENTÍMETRO')
-    return input('Informe a unidade de medida: ')
-    
 def criaMedidas(c):
-    if c == 1:
+    if c == 1: #Medidas do cômodo
         lado1 = float(input('Lado 1 (m) = '))
         lado2 = float(input('Lado 2 (m) = '))
-    else:
+    else: #Medidas de cada azulejo
         lado1 = float(input('Lado 1 (cm) = '))
         lado2 = float(input('Lado 2 (cm) = '))
-        lado1 /= 100
+        lado1 /= 100 #Converte para metro
         lado2 /= 100
     print()
     return lado1, lado2
     
 def criaObjeto(lado1, lado2):
     if lado1 == lado2:
-        obj = Quadrado(lado1)
+        obj = Quadrado(lado1) #Azulejo sempre será quadrado
     else:
-        obj = Retangulo(lado1, lado2)
+        obj = Retangulo(lado1, lado2) #Cômodo pode ser quadrado ou retângulo
     return obj
     
 def mostraDados(lado1, lado2, obj):
@@ -51,21 +46,21 @@ def mostraDados(lado1, lado2, obj):
     print(f'--> Área total = {obj.calculaArea():.2f}m2')
     print()
 
-def totalAzulejo(lista):
-    print(f'Serão necessários {ceil(lista[0] / lista[1])} azulejos.') 
+def totalAzulejo(lista): #Calcula quantidade de azulejos para revestir o piso do cômodo
+    print(f'Serão necessários {ceil(lista[0] / lista[1])} azulejos.') #O primeiro valor da lista é a área do cômodo e o segundo é a área de cada azulejo
     
 def main():
     ListaAreas = []
-    for contador in range(1, 3):
-        if contador == 1:
+    for contador in range(1, 3): #Repete duas vezes, uma para o cômodo e outra para o azulejo
+        if contador == 1: #Vez do cômodo
             topo('CÔMODO')
-        else:
+        else: #Vez do azulejo
             topo('AZULEJO')
         Lado1, Lado2 = criaMedidas(contador)
-        objeto = criaObjeto(Lado1, Lado2)
+        objeto = criaObjeto(Lado1, Lado2) #Cria objeto do tipo quadrado ou retângulo
         mostraDados(Lado1, Lado2, objeto)
-        ListaAreas.append(objeto.calculaArea())
-    topo(qtd = 30)
+        ListaAreas.append(objeto.calculaArea()) #Acumula áreas dos dois objetos criados em cada repetição; necessário para calcular o total de azulejos
+    topo(qtd = 35)
     totalAzulejo(ListaAreas)
 
 if __name__ == '__main__':
