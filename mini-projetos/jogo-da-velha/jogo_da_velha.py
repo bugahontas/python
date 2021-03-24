53a54fbb53360fce4967ebc368ec59f23935832e
Helena M <contatohelmaqui@gmail.com>
2021-03-24 13:39:46 -0300

    Update jogo_da_velha.py

--- mini-projetos/jogo-da-velha/jogo_da_velha.py
+++ mini-projetos/jogo-da-velha/jogo_da_velha.py
@@ -34,25 +34,56 @@ def escolhe_posicao(jogador, ja_escolhidas):
             escolha = input('Escolha outra posição: ')
             print()
             
-def analisa_posicoes():
+def analisa_horizontais(posicoes):
     inicio = 0
     fim = 3
     X_total = 0
     O_total = 0
 
-    for p in range(inicio, fim):
-        if p == 'X':
-            X_total += 1
-        elif p == 'O':
-            O_total += 1
-    if X_total == 3:
-        return 'JOGADOR 1 VENCE!'
-    elif O_total == 3:
-        return 'JOGADOR 2 VENCE!'
+    while fim <= 9:
+        for p in range(inicio, fim):
+            if posicoes[p] == 'X':
+                X_total += 1
+            elif posicoes[p] == 'O':
+                O_total += 1
+        if X_total == 3:
+            return 'JOGADOR 1 VENCE!'
+        elif O_total == 3:
+            return 'JOGADOR 2 VENCE!'
 
-    inicio = fim
-    fim += 3 
+        inicio = fim
+        fim += 3 
+        X_total = 0
+        O_total = 0
 
+    if fim > 9:
+        return ''
+
+def analisa_verticais(posicoes):
+    inicio = 0
+    fim = 6
+    X_total = 0
+    O_total = 0
+
+    while fim <= 8:
+        for p in range(inicio, fim + 1, 3):
+            if posicoes[p] == 'X':
+                X_total += 1
+            elif posicoes[p] == 'O':
+                O_total += 1
+        if X_total == 3:
+            return 'JOGADOR 1 VENCE!'
+        elif O_total == 3:
+            return 'JOGADOR 2 VENCE!'
+
+        inicio += 1 
+        fim += 1 
+        X_total = 0
+        O_total = 0
+
+    if fim > 8:
+        return ''
+    
     
 
 def main():
@@ -62,23 +93,28 @@ def main():
 
     Vez = 1
     Ja_escolhidas = []
+    Resultado = ''
     
-    while True:
+    while Resultado == '':
 
         if Vez % 2 != 0:
             Escolha = escolhe_posicao('JOGADOR 1', Ja_escolhidas)
             Posicoes[Escolha - 1] = 'X'
+            Resultado = analisa_horizontais(Posicoes)
+            Resultado = analisa_verticais(Posicoes)
         else:
             Escolha = escolhe_posicao('JOGADOR 2', Ja_escolhidas)
             Posicoes[Escolha - 1] = 'O'
+            Resultado = analisa_horizontais(Posicoes)
+            Resultado = analisa_verticais(Posicoes)
 
         mostra_tabuleiro(Posicoes)
 
         Vez += 1
 
-        
-
-
+    print()
+    print()
+    print(Resultado)
 
 if __name__ == '__main__':
     main()
