19bde52a85a3906c0e2ed1c14a7ce4083f9160b4
Helena M <contatohelmaqui@gmail.com>
2021-04-01 15:21:10 -0300

    create calculadora

---
+++ mini-projetos/calculadora-com-conversores/calculadora_com_conversores.py
@@ -0,0 +1,26 @@
+def menu_geral():
+    print('Arredondamento Tecle []')
+    print('Área        Tecle []')
+    print('Conversores Tecle []') #Incluir angulos
+    print('Equação de 2o. Grau ------- Tecle []')
+    print('Fatorial ------------------ Tecle []')
+    print('Inteiro ou decimal -------- Tecle []')
+    print('Inverso Tecle []')
+    print('Logaritmo Tecle []')
+    print('Médias Tecle []')
+    print('Operações aritméticas       Tecle []')
+    print('Operações trigonométricas   Tecle []')
+    print('Ordem crescente Tecle []')
+    print('Ordem decrescente Tecle []')
+    print('Parte inteira Tecle []')
+    print('Porcentagem Tecle []')
+    print('Potenciação Tecle []')
+    print('Radiciação Tecle []')
+    print('Valor absoluto Tecle []')
+    print('Valor de pi Tecle []')
+    print('Volume Tecle []')
+
+def arredondamento():
+    valor = float(input('Digite um valor decimal: '))
+    
+    
