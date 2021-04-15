d4c46e24231fdb407bc0dff141f9805ae0b08dc6
bugahontas <contatohelmaqui@gmail.com>
2021-04-15 13:58:37 -0300

    Update zerinho_ou_um.py

--- mini-projetos/zerinho-ou-um/zerinho_ou_um.py
+++ mini-projetos/zerinho-ou-um/zerinho_ou_um.py
@@ -1,3 +1,29 @@
+'''Autora: Helena Maruf'''
+'''Data: 15/03/2021'''
+'''Nome do arquivo: zerinho_ou_um.py'''
+'''Descrição: este código é uma implementação em modo texto do jogo clássico "Zerinho ou Um."'''
+'''Funcionamento:
+    --> O usuário escolhe com quantos amigos quer jogar e o nome de cada amigo.
+    --> O usuário deve informar 0 ou 1.
+    --> Cada amigo criado também escolherá 0 ou 1 de forma aleatória.
+    --> Vence quem escolher um número diferente de todos os outros participantes.
+        --> Se não houver apenas uma pessoa com 0 ou uma pessoa com 1, a rodada será repetida até que alguém vença.
+    --> Exemplos de rodada:
+            1. Você escolhe jogar com dois amigos: Ana e Bia.
+            2. Situação 1: Você escolhe "0"; Ana escolhe "0"; Bia escolhe "1".
+            3. Bia foi a única que escolheu 1. Logo, a vencedora é Bia.
+
+            1.1 Supondo que você queira jogar de novo com Ana e Bia.
+            1.2 Situação 2: Você escolhe "0"; Ana escolhe "1"; Bia escolhe "1".
+            1.3 Você foi o único que escolheu 0. Logo, você vence.
+
+            2.1 Supondo que você queira jogar de novo, mas dessa vez com Ana, Bia e Daniel.
+            2.2 Situação 3: Você escolhe "0"; Ana escolhe "1"; Bia escolhe "1" e Daniel escolhe "0".
+            2.3 Tanto o 0 quanto o 1 foram escolhidos por mais de uma pessoa no grupo.
+            2.4 Então há uma nova rodada: Você escolhe "1"; Ana escolhe "1"; Bia escolhe "1" e Daniel escolhe "0"
+            2.5 Logo, Daniel vence.'''
+
+
 from random import randint
 
 
