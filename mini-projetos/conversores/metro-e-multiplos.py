2d83eca9e69babc33e466a05d1c13c086586e152
bugahontas <contatohelmaqui@gmail.com>
2021-04-30 16:49:04 -0300

    header-metro-e-multiplos

--- mini-projetos/conversores/metro-e-multiplos.py
+++ mini-projetos/conversores/metro-e-multiplos.py
@@ -1,3 +1,21 @@
+'''Autor: Helena Maruf'''
+'''Data: 30/04/2021'''
+'''Nome do arquivo: metro_e_multiplos.py'''
+'''Descrição: este código converte um valor em uma unidade para múltiplos e submúltiplos do metro.'''
+'''Funcionamento:
+    --> Informe um valor.
+    --> Informe a unidade de medida desse valor.
+    --> Informe a unidade de medida para a qual deseja converter o valor.
+    --> O resultado irá conter o valor com a unidade convertidos em potência de 10.
+    --> Exemplo de uso:
+        1. Você deseja converter 1 metro para centímetros.
+        2. Você informa o valor 1.
+        3. Em seguida, você informa a unidade atual que é metro.
+        4. Você também informa a unidade para a qual deseja converter (no caso, centímetro).
+        5. O código retorna como resultado "1m = 1 x 10^2cm" (lê-se "1 metro vale 1 vezes 10 elevado a 2 centímetros").'''
+
+
+
 def menu(caractere = '=', total = 27):
     linha = caractere * total
     print(linha)
